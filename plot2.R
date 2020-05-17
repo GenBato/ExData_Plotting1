ade389dc4600a7afb3d183bbf5e0b19ace13434a
GenBato <59519641+GenBato@users.noreply.github.com>
2020-05-18 07:39:04 +0800

    PLOT 2-4

---
+++ plot2.R
@@ -0,0 +1,10 @@
+x<-read.table("household_power_consumption.txt",header=T,sep=";")
+head(x)
+dim(x)
+x$Date <- as.Date(x$Date, format="%d/%m/%Y")
+y <- x[(x$Date=="2007-02-01") | (x$Date=="2007-02-02"),]
+y$Global_active_power <- as.numeric(as.character(y$Global_active_power))
+y <- transform(y, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+plot(y$timestamp,y$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.copy(png, file="plot2.png", width=480, height=480)
+dev.off()
